dcbbb492072aab523ba257185e975d46efa278e2
unknown <kleinolivier@orange.fr>
2023-07-11 13:37:44 +0200

    :construction: Amélioration de server.js

--- server.js
+++ server.js
@@ -1,7 +1,49 @@
 const http = require('http');
 const app = require('./app');
 
-app.set('port', process.env.PORT || 3000);
-const server = http.createServer(app); 
+const normlizePort = val => {
+    const port = parseInt(val, 10);
 
-server.listen(process.env.PORT || 3000);
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+};
+
+const port = normlizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + 'requires extended privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on ('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('listening on' + bind);
+});
+
+server.listen(port);
