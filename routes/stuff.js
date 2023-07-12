757ed1510f4a466e7c7d364696c9bc50db365490
Olivier <kleinolivier@orange.fr>
2023-07-12 11:26:06 +0200

    :bug: correction d'une route qui ne fonctionnait pas correctemment

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const router = express.Router();
 
 const Thing = require('../models/thing');
@@ -32,7 +31,7 @@ router.get('/:id', (req, res, next) =>{
     .catch(error => res.status(404).json ({error}));
 });
 
-router.get('/api/stuff', (req, res, next) => {
+router.get('/', (req, res, next) => {
     Thing.find()
     .then(things => res.status(200).json(things))
     .catch(error => res.status(400).json ({error}));
