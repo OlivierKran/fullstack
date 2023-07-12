22beb56ad46a45b686bb917c57c3d578fe050836
Olivier <kleinolivier@orange.fr>
2023-07-12 11:24:09 +0200

    :memo: commentaires sur les cors

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ useUnifiedTopology: true})
 
 const app = express();
 
+//gestion des CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
