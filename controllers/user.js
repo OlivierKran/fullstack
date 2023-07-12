3d052481331a89b71f2a1b26827930d0e5c121b0
Olivier <kleinolivier@orange.fr>
2023-07-12 15:47:07 +0200

    :construction: connexion/création user completed

--- controllers/user.js
+++ controllers/user.js
@@ -17,5 +17,29 @@ bcrypt.hash(req.body.password, 10)
 };
 
 exports.login = (req, res, next) => {
+    User.findOne({email: req.body.email})
+    .then(user => {
+        if(user === null){
+            res.status(401).json({message: 'Paire identifiant/mot de passe incorrecte'})        
+        }else{
+            bcrypt.compare(req.body.password, user.password)
+            .then(valid =>{
+                if(!valid) {
+                    res.status(401).json({message: "Mot de passe incorrect !"})
+                } else {
+                    res.status(200).json({
+                        userId: user._id,
+                        token: 'TOKEN'
+                    });
+                }
 
+            })
+            .catch(error =>{
+                res.status(500).json ({ error });
+            })
+        }
+    })
+    .catch(error => {
+        res.status(500).json( { error })
+    })
 };
